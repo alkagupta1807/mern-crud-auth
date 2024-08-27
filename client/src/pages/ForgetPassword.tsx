812ee8a1ab591ceea801b0cd8ef7f57ad491c29a
alkagupta1807 <141111904+alkagupta1807@users.noreply.github.com>
2024-08-27 19:07:32 +0530

    Update ForgetPassword.tsx

--- client/src/pages/ForgetPassword.tsx
+++ client/src/pages/ForgetPassword.tsx
@@ -17,7 +17,7 @@ const ForgetPassword = () => {
   const onSubmit = async (data: { email: string }) => {
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:4000/api/auth/forget-password', {
+      const response = await fetch('https://mern-crud-auth-hzrx.onrender.com/api/auth/forget-password', {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
