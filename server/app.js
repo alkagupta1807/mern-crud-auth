1bfeaa8f18e2003f42357357a4095e0cd151438f
alkagupta1807 <141111904+alkagupta1807@users.noreply.github.com>
2024-08-27 16:31:58 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({ extended: true,limit:"50mb" }));
 app.use(cors(
   {
-    origin:process.env.FRONTEND_URL,
+    origin:"https://mern-crud-auth-hzrx.onrender.com",
     credentials:true
   }
 ));
